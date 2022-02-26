e661e38c3ce67a177855885833290ae35fe2d862
Rods <rodrigogregoldo@gmail.com>
2022-02-26 18:49:00 -0300

    GET NEXT LINE IS WORKING JUST FINE, main is mocked to test it, revert that l8r allig8r

--- cli/include/neutraliza-cli.h
+++ cli/include/neutraliza-cli.h
@@ -6,40 +6,57 @@
 # include <stdio.h>		// dprintf()
 # include <stdlib.h>	// error()
 # include <string.h>	// strcmp()
+# include <stdarg.h>	// vargs
+# include <unistd.h>	// needed for get_next_line()
+
+# define	BUFFER_SIZE	100
 
 // ------------------------------------------------		TYPEDEFS	-----------
 
 typedef struct s_exec_flags
 {
 	int	opt_d;	// default = 0;		display dashboard;
-	int opt_h;	// default = 0;		display help msg;
-	int opt_a;	// default = 0;		log type: all;
-	int type_a;	// default = 0;		log type: all;
-	int type_g;	// default = 0;		log type: GET;
-	int type_p;	// default = 0;		log type: POST;
-	int type_i;	// default = 0;		log type: INFO;
-	int type_e;	// default = 0;		log type: ERROR;
-	int form_m;	// default = 0;		format: minified;
-	int form_b;	// default = 0;		format: bbbeeeautifulll,ø¤°º¤ø,¸¸,ø¤º°º¤
+	int	opt_h;	// default = 0;		display help msg;
+	int	opt_a;	// default = 0;		log type: all;
+	int	type_a;	// default = 0;		log type: all;
+	int	type_g;	// default = 0;		log type: GET;
+	int	type_p;	// default = 0;		log type: POST;
+	int	type_i;	// default = 0;		log type: INFO;
+	int	type_e;	// default = 0;		log type: ERROR;
+	int	form_m;	// default = 0;		format: minified;
+	int	form_b;	// default = 0;		format: bbbeeeautifulll,ø¤°º¤ø,¸¸,ø¤º°º¤
 }	t_exec_flags;
+
+typedef struct s_endpoint
+{
+	char	*uri;
+	char	*method;
+	int		tot_reqs;
+}	t_endpoint;
 
 // ------------------------------------------------		PROTOTYPES	-----------
 // utils_exec.c
-void	execute_cmd(t_exec_flags *exec_flags);
+void		execute_cmd(t_exec_flags *exec_flags);
 
 // utils_init.c
-void	init_flags(t_exec_flags *exec_flags);
+void		init_flags(t_exec_flags *exec_flags);
 
 // utils_libft.c
-char	*ft_strchr(const char *s, int c);
+char		*ft_strchr(const char *s, int c);
+
+// utils_libft.c
+int 		count_logs(const char *log_type, ...);
 
 // utils_parsing.c
-void	capture_flags(int argc, char *argv[], t_exec_flags *exec_flags);
-void	check_flags_consistency(t_exec_flags *exec_flags);
+void		capture_flags(int argc, char *argv[], t_exec_flags *exec_flags);
+void		check_flags_consistency(t_exec_flags *exec_flags);
 
 // utils_print.c
-void	display_dashboard(void);
-void	print_help_msg_exit(void);
-void	print_help_menu(void);
+void		print_help_msg_exit(void);
+void		print_help_menu(void);
+void		display_dashboard(void);
+
+// utils_strings.c
+char	*append_n_strs(char* body, int count_append, char *appendage, ...);
 
 #endif
--- cli/src/neutraliza-cli.c
+++ cli/src/neutraliza-cli.c
@@ -11,13 +11,50 @@ void	parse_input(int argc, char *argv[])
 	return ;
 }
 
-int	main(int argc, char *argv[])
+// int	main(int argc, char *argv[])
+// {
+// 	if (argc < 1)
+// 		print_help_msg_exit();
+// 	else if (argc == 1)
+// 		display_dashboard();
+// 	else
+// 		parse_input(argc, argv);
+// 	return (0);
+// }
+
+#include <stdio.h>	// FILE, fopen()
+#include <stdio.h> // printf()
+#include <stdlib.h> // free()
+
+char	*g_file_name = "/home/rods/documents/git/42labs-selection-process-v2-rodsmade/logs/logs.txt";
+FILE	*g_fp;
+
+char	*get_next_line(int fd);
+
+int main()
+/**
+*	- Function main() opens a file in a file descriptor (fd / _fileno).
+*	- This file contains text meant to be read one line at a time by means
+*	of function get_next_line().
+*	- Each call must return a string of chars ended tied up with a '\n', so long
+*	as there is one. Empty lines are returned all the same (one single '\n' per call).
+*	- main() proceeds to print the resulting line read by get_next_line(),
+*	looping throughout the file.
+*	- Once EOF is reached, get_next_line() returns NULL and stops execution;
+*	main() closes the file, and the program comes to a halt.
+**/
 {
-	if (argc < 1)
-		print_help_msg_exit();
-	else if (argc == 1)
-		display_dashboard();
-	else
-		parse_input(argc, argv);
+	char	*linha;
+	int		i = 0;
+
+	g_fp = fopen(g_file_name, "r");
+	
+	while((linha = get_next_line(g_fp->_fileno)) != NULL)
+	{
+		printf("Line %i >>> %s", i++, linha);
+		free(linha);
+	}
+	
+	fclose(g_fp);
 	return (0);
 }
